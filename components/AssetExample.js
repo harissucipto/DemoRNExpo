579329b678f07e4f2e3dfcaf04ae9ca22bd20374
harissucipto <haris.sucipto@mitrais.com>
2025-11-19 23:11:48 +0800

    fix: update text in AssetExample component for clarity

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -4,7 +4,7 @@ export default function AssetExample() {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        Local files and assets can be imported by dragging and dropping them into the editor
+        Local files and assets can be imported by dragging and dropping them into the editor as well.
       </Text>
       <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
     </View>
